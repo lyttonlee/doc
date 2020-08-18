35fa363a3d7b1bb1c158538e65263c4f058c0395
lizairong <lzr3278@163.com>
2020-08-18 17:19:09 +0800

    添加可跨域访问

--- src/main/java/cool/lytton/doc/DocApplication.java
+++ src/main/java/cool/lytton/doc/DocApplication.java
@@ -1,9 +1,11 @@
 package cool.lytton.doc;
 
+import cool.lytton.doc.config.CorsConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//@EnableAutoConfiguration(exclude = {CorsConfig.class})
 @SpringBootApplication
 @MapperScan("cool.lytton.doc.mapper")
 public class DocApplication {
