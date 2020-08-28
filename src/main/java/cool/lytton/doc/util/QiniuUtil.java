8c2af34bcc8b30a0d35b0e257eaa7bba71875146
lizairong <lzr3278@163.com>
2020-08-28 10:23:59 +0800

    修改默认存储的七牛云空间

--- src/main/java/cool/lytton/doc/util/QiniuUtil.java
+++ src/main/java/cool/lytton/doc/util/QiniuUtil.java
@@ -5,7 +5,7 @@ import com.qiniu.util.Auth;
 public class QiniuUtil {
     private static String AK = "YOXpF0XvM_3yVDsz5C-hWwrFE5rtDAUQC3XjBQEG";
     private static String SK = "CmrhUV2xHf1d8nPCsws9wwm7jKypCPA0lRVm-7lS";
-    private static String bucket = "doc-article-image";
+    private static String bucket = "test-editor";
 
     public static String createToken() {
         try {
